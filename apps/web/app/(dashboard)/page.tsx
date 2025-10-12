1ca7ecef1981eba64eebbd8a1d399f619b411cc3
AdylshaY <adylshay@gmail.com>
2025-10-12 20:08:42 +0300

    dashboard layout created and shadcn components installed

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -10,17 +10,14 @@ export default function Page() {
   const addUser = useMutation(api.users.add);
 
   return (
-    <>
-      <div className='flex flex-col items-center justify-center min-h-svh'>
-        <p>Apps Web</p>
-        <UserButton />
-        <OrganizationSwitcher hidePersonal />
-        <Button onClick={() => addUser({ name: 'New User' })}>Add</Button>
-        <div className='max-w-sm w-full mx-auto'>
-          {JSON.stringify(users, null, 2)}
-        </div>
+    <div className='flex flex-col items-center justify-center min-h-svh w-screen'>
+      <p>Apps Web</p>
+      <UserButton />
+      <OrganizationSwitcher hidePersonal />
+      <Button onClick={() => addUser({ name: 'New User' })}>Add</Button>
+      <div className='max-w-sm w-full mx-auto'>
+        {JSON.stringify(users, null, 2)}
       </div>
-      <p>Must be authenticated to view this content</p>
-    </>
+    </div>
   );
 }
