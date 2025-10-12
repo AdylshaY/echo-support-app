aa7249f4b0e1c05aaef014021d254c2453e2e41b
AdylshaY <adylshay@gmail.com>
2025-10-12 22:03:11 +0300

    basic widget view created

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,20 +1,15 @@
 'use client';
+import { WidgetView } from '@/modules/ui/views/widget-view';
+import { use } from 'react';
 
-import { useMutation, useQuery } from 'convex/react';
-import { api } from '@workspace/backend/_generated/api';
-import { Button } from '@workspace/ui/components/button';
-
-export default function Page() {
-  const users = useQuery(api.users.getMany);
-  const addUser = useMutation(api.users.add);
-
-  return (
-    <div className='flex flex-col items-center justify-center min-h-svh'>
-      <p>Apps Web</p>
-      <Button onClick={() => addUser({ name: 'New User' })}>Add</Button>
-      <div className='max-w-sm w-full mx-auto'>
-        {JSON.stringify(users, null, 2)}
-      </div>
-    </div>
-  );
+interface Props {
+  searchParams: Promise<{ organizationId: string }>;
 }
+
+const Page = ({ searchParams }: Props) => {
+  const { organizationId } = use(searchParams);
+
+  return <WidgetView organizationId={organizationId} />;
+};
+
+export default Page;
