a7c401d2c75f4749dafaee72171896e28369668b
Adylsha Yumayev <adylsha.yumayev@gelecekvarlik.com.tr>
2025-10-14 13:58:11 +0300

    conversation getOne security fix

--- packages/backend/convex/public/conversations.ts
+++ packages/backend/convex/public/conversations.ts
@@ -18,7 +18,19 @@ export const getOne = query({
 
     const conversation = await ctx.db.get(args.conversationId);
 
-    if (!conversation) return null;
+    if (!conversation) {
+      throw new ConvexError({
+        code: 'NOT_FOUND',
+        message: 'Conversation not found',
+      });
+    }
+
+    if (conversation.contactSessionId !== session._id) {
+      throw new ConvexError({
+        code: 'UNAUTHORIZED',
+        message: 'Incorrect session',
+      });
+    }
 
     return {
       _id: conversation._id,
